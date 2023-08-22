0560db23c2cc8f815addfbc3ff51a073237968aa
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-08-22 16:24:19 -0300

    add-layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,16 @@
 import { Header } from "./Header";
 import { Modal } from "./Modal";
 import { Menu } from "./Menu";
+import { Footer } from "./Footer";
 
 export const Layout = ({ children }: any) => {
 	return (
 		<div>
+			<Modal />
 			<Header />
 			<Menu />
 			{children}
-			{/* <Modal /> */}
-			{/* <Footer /> */}
+			<Footer />
 		</div>
 	)
 }
