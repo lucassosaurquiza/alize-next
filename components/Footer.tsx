68be721c3161bc487ae4626fc9b4a2769aa9f727
Alexispavon010 <themaster034@gmail.com>
2023-08-22 18:16:36 -0300

    add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
-
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-red-400'>
+      <div className="max-w-7xl m-auto">
+        <div className='text-white lg:pl-56'>
+          <div className="flex p-3 justify-between flex-wrap gap-5 w-full md:flex md:flex-row">
+            <div className="mb-5">
+              <h2 className="w-full mb-2">Empresa</h2>
+              <div className="text-xs mb-1">¿Quienes Somos?</div>
+              <div className="text-xs mb-1">Politica de Privacidad</div>
+              <div className="text-xs">Preferencias</div>
+            </div>
+            <div className="mb-5 ">
+              <h2 className="mb-2">Contacto</h2>
+              <div className="text-xs mb-1">¿Quienes Somos?</div>
+              <div className="text-xs mb-1">Politica de Privacidad</div>
+              <div className="text-xs">Preferencias</div>
+            </div>
+            <div className=" ">
+              <h2 className="mb-2">Redes</h2>
+              <div className="text-xs mb-1">¿Quienes Somos?</div>
+              <div className="text-xs mb-1">Politica de Privacidad</div>
+              <div className="text-xs">Preferencias</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
