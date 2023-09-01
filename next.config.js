727759898cbc421f8934865b46fa9889eac71ae3
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-09-01 19:33:42 -0300

    new-configurations

--- next.config.js
+++ next.config.js
@@ -4,3 +4,14 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+const path = require('path');
+
+module.exports = {
+  // ...
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
