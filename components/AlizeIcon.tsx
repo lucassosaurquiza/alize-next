b3e24f626a59dd0bef02ba3f6d23a1d918e0488f
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-09-14 12:59:13 -0300

    add-icon-alize

--- components/AlizeIcon.tsx
+++ components/AlizeIcon.tsx
@@ -1,7 +1,7 @@
 export const AlizeIcon = () => {
   return (
-   <div className='flex justify-center items-center text-center'>
-    <span className='text-red-400 font-bold text-xl'>alize</span>
-   </div>
+    <div className='flex justify-center items-center text-center'>
+      <img className="h-auto w-16 block m-auto" src="assets/Alize-logo(1).png" alt="" />
+    </div>
   )
 }
